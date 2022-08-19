ab667db35c11f0e282b4a83288015b35748dafb5
hongmj37 <alswjd3813@naver.com>
2022-08-19 10:00:32 +0900

    Spring MVC 실습

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -21,8 +21,30 @@ public class HelloController {
     }
 
     @GetMapping("hello-string")
-    @ResponseBody
+    @ResponseBody  //이 애너테이션이 붙으면 template를 찾아가지 않고 그대로 반환
     public String helloString(@RequestParam("name") String name) {
-        return "hello " + name;
+        return "hello " + name;  //문자면 문자 그대로 반환
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello(); //command + shift + Enter 하면
+        hello.setName(name);
+        return hello; //객체이면 json 구조로 나타남 {"name" : "spring!!"}
+    } //viewResolver 대신 HttpMessageConverter가 대신 동작
+       //문자 처리 : StringHttpMessageConverter
+       //객체 처리 : MappingJackson2HttpMessageConverter
+
+    static class Hello {  //static으로 만들면 HelloController 클래스 안에서 클래스를 또 만들 수 있음
+        private String name;
+        //name이 private이기 때문에 getter/setter 해줌
+        public String getName() {  //command + N 단축키로 getter setter 쉽게 만들 수 있음
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
