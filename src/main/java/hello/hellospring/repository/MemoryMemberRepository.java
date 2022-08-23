45712f8e2ad58e02d971bb4817c6a68fc4b915bd
hongmj37 <alswjd3813@naver.com>
2022-08-23 13:38:34 +0900

    Spring 실습

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -2,32 +2,37 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class MemoryMemberRepository implements MemberRepository{  //Option + Enter 하면 MemberRepository 에 있는 메서드를 전부 가져올 수 있음
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L; //키값을 생성해주는 녀석
-    
+
     @Override
     public Member save(Member member) {
-        return null;
+         member.setId(++sequence);
+         store.put(member.getId(), member);
+         return member;
     }
 
     @Override
     public Optional<Member> findById(Long id) {
-        return Optional.empty();
+        return Optional.ofNullable(store.get(id));
     }
 
     @Override
     public Optional<Member> findByName(String name) {
-        return Optional.empty();
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
     }
 
     @Override
     public List<Member> findAll() {
-        return null;
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
     }
 }
